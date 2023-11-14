e1726fddce92dd0cdabadd37495ed6e27e1c36b1
Kushagradalal1 <kushagradalal33@gmail.com>
2023-11-14 12:25:03 +0530

    Exception Handling Notes By Kushagra

---
+++ src/ExceptionHandlingExplain/Explain.java
@@ -0,0 +1,79 @@
+package ExceptionHandlingExplain;
+
+public class Explain {
+    /*
+
+    : Exception Handling is an abnormal condition (exceptional condition) that may
+    occurs during the exception of program and if your program is not ready
+    to handle it then it will be abnormally terminated.
+
+    : making your program ready to deal with these abnormal
+    condition is exception handling.
+
+    : program to read a file from the disk :
+    : you are providing the file name and its reading the file into
+    a disk : output showing the contents of the file :
+
+    : if suppose you want to open the file called xyz.txt and that
+    file is not present int the disk then, that is exception : that is
+    : FileNotFoundException : to control this exception that is known as handling of
+    an exception.
+
+    : How to handle the exception :
+    there are 5 keywords given in java : try, catch, finally, throw & throws.
+
+    : try, catch & finally are the blocks : blocks means groups of statements :
+
+    : way to write this blocks :
+    //One try and One Catch :
+    1) try{
+
+    }catch{
+
+    }
+    ------------------------------
+    2) try{
+
+    }catch{
+
+    }catch{
+
+    }
+    .
+    .
+    .
+    ------------------------------
+    3) try{
+
+    }catch{
+
+    }finally{
+
+    }
+    ------------------------------
+    4) try{
+
+    }catch{
+
+    }catch{
+
+    }catch{
+
+    }
+    .
+    .
+    .
+    finally{
+
+    }
+    ------------------------------
+    5) try{
+
+    }finally{
+
+    }
+    ------------------------------
+
+
+     */
+}
